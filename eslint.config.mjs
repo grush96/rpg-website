9b4bac8548c8c0b25a1820d7885c65aa98c9459a
grush96 <56136982+grush96@users.noreply.github.com>
2024-11-14 12:08:29 -0800

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    "extends":  [
+      "next/core-web-vitals", 
+      "next/typescript", 
+      "next", 
+      "prettier"
+    ]
+  },
+  eslintConfigPrettier
+];
