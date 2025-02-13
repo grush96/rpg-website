3bf709fa282bbe637afbeaec8e7cb112e85753ef
Gabe_Laptop <git.wildcat129@simplelogin.com>
2025-02-13 10:56:15 -0800

    Add toggle for point buy in knowledge selection

--- src/components/charactercreator/knowledge-select.tsx
+++ src/components/charactercreator/knowledge-select.tsx
@@ -14,7 +14,10 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ScrollArea } from "../ui/scroll-area";
+import { Toggle } from "@/components/ui/toggle";
 
+// TODO: consider adding dropdown option for info categories (disable with hover before buy)
+// TODO: figure out levels for info, may change design
 export default function KnowledgeSelect() {
   const infos = [
     {
@@ -59,6 +62,120 @@ export default function KnowledgeSelect() {
     },
   ] as const;
 
+  const langs1 = [
+    {
+      id: "common",
+      label: "Common",
+    },
+    {
+      id: "construct",
+      label: "Construct",
+    },
+    {
+      id: "damned",
+      label: "Damned",
+    },
+    {
+      id: "primordial",
+      label: "Primordial",
+    },
+  ];
+
+  const langs2 = [
+    {
+      id: "celestial",
+      label: "Celestial",
+    },
+    {
+      id: "eldritch",
+      label: "Eldritch",
+    },
+    {
+      id: "infernal",
+      label: "Infernal",
+    },
+    {
+      id: "sylvan",
+      label: "Sylvan",
+    },
+  ];
+
+  function printLang(langs: Array<{ id: string; label: string }>) {
+    return (
+      <div className="grid grid-cols-7 justify-items-center items-center gap-y-1">
+        <p className="col-span-3"></p>
+        <p>R</p>
+        <p>W</p>
+        <p>L</p>
+        <p>S</p>
+        {langs.map((lang) => (
+          <>
+            <p className="col-span-3 justify-self-start">{lang.label}</p>
+            <Toggle
+              id={lang.id + "-r"}
+              onPressedChange={(pressed) => {
+                if (pressed && document) {
+                  console.log(lang.id + "-w");
+                  document
+                    .getElementById(lang.id + "-w")
+                    ?.removeAttribute("disabled");
+                  document
+                    .getElementById(lang.id + "-w")
+                    ?.removeAttribute("data-disabled");
+                } else if (!pressed && document) {
+                  console.log("unpressed");
+                  document
+                    .getElementById(lang.id + "-w")
+                    ?.setAttribute("disabled", "true");
+                  document
+                    .getElementById(lang.id + "-w")
+                    ?.setAttribute("data-disabled", "true");
+                }
+              }}
+            >
+              1
+            </Toggle>
+            <Toggle id={lang.id + "-w"}>1</Toggle>
+            <Toggle
+              id={lang.id + "-l"}
+              onPressedChange={(pressed) => {
+                if (pressed && document) {
+                  console.log(lang.id + "-s");
+                  document
+                    .getElementById(lang.id + "-s")
+                    ?.removeAttribute("disabled");
+                  document
+                    .getElementById(lang.id + "-s")
+                    ?.removeAttribute("data-disabled");
+                } else if (!pressed && document) {
+                  console.log("unpressed");
+                  // document
+                  //   .getElementById(lang.id + "-s")
+                  //   ?.setAttribute("disabled", "true");
+                  // document
+                  //   .getElementById(lang.id + "-s")
+                  //   ?.setAttribute("data-disabled", "true");
+                  document
+                    .getElementById(lang.id + "-s")
+                    ?.setAttribute("aria-pressed", "false");
+                  document
+                    .getElementById(lang.id + "-s")
+                    ?.setAttribute("data-state", "off");
+                  document
+                    .getElementById(lang.id + "-s")
+                    ?.setAttribute("pressed", "off");
+                }
+              }}
+            >
+              1
+            </Toggle>
+            <Toggle id={lang.id + "-s"}>1</Toggle>
+          </>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="grid gap-3">
       <Label htmlFor="knowledge" className="text-center">
@@ -74,8 +191,9 @@ export default function KnowledgeSelect() {
             <DialogHeader>
               <DialogTitle>Knowledge Selection</DialogTitle>
               <DialogDescription>
-                Choose what knowledge (including languages, skills, and magic)
-                your character knows. Click save when you're done.
+                Choose what knowledge (including languages, information, skills,
+                and magic) your character knows. The number inside toggles are
+                the point cost. Click save when you're done.
               </DialogDescription>
             </DialogHeader>
             <div className="grid gap-4 py-4">
@@ -88,60 +206,8 @@ export default function KnowledgeSelect() {
                   listen (L), and/or speak (S).
                 </DialogDescription>
                 <div className="grid grid-cols-1 min-[400px]:grid-cols-2 gap-x-3">
-                  <div className="grid grid-cols-7 justify-items-center items-center">
-                    <p className="col-span-3"></p>
-                    <p>R</p>
-                    <p>W</p>
-                    <p>L</p>
-                    <p>S</p>
-                    <p className="col-span-3 justify-self-start">Common</p>
-                    <Checkbox key="common-r" />
-                    <Checkbox key="common-w" />
-                    <Checkbox key="common-l" />
-                    <Checkbox key="common-s" />
-                    <p className="col-span-3 justify-self-start">Construct</p>
-                    <Checkbox key="construct-r" />
-                    <Checkbox key="construct-w" />
-                    <Checkbox key="construct-l" />
-                    <Checkbox key="construct-s" />
-                    <p className="col-span-3 justify-self-start">Damned</p>
-                    <Checkbox key="damned-r" />
-                    <Checkbox key="damned-w" />
-                    <Checkbox key="damned-l" />
-                    <Checkbox key="damned-s" />
-                    <p className="col-span-3 justify-self-start">Primordial</p>
-                    <Checkbox key="primordial-r" />
-                    <Checkbox key="primordial-w" />
-                    <Checkbox key="primordial-l" />
-                    <Checkbox key="primordial-s" />
-                  </div>
-                  <div className="grid grid-cols-7 justify-items-center items-center">
-                    <p className="col-span-3"></p>
-                    <p>R</p>
-                    <p>W</p>
-                    <p>L</p>
-                    <p>S</p>
-                    <p className="col-span-3 justify-self-start">Celestial</p>
-                    <Checkbox key="celestial-r" />
-                    <Checkbox key="celestial-w" />
-                    <Checkbox key="celestial-l" />
-                    <Checkbox key="celestial-s" />
-                    <p className="col-span-3 justify-self-start">Eldritch</p>
-                    <Checkbox key="eldritch-r" />
-                    <Checkbox key="eldritch-w" />
-                    <Checkbox key="eldritch-l" />
-                    <Checkbox key="eldritch-s" />
-                    <p className="col-span-3 justify-self-start">Infernal</p>
-                    <Checkbox key="infernal-r" />
-                    <Checkbox key="infernal-w" />
-                    <Checkbox key="infernal-l" />
-                    <Checkbox key="infernal-s" />
-                    <p className="col-span-3 justify-self-start">Sylvan</p>
-                    <Checkbox key="sylvan-r" />
-                    <Checkbox key="sylvan-w" />
-                    <Checkbox key="sylvan-l" />
-                    <Checkbox key="sylvan-s" />
-                  </div>
+                  {printLang(langs1)}
+                  {printLang(langs2)}
                 </div>
               </fieldset>
               <fieldset className="grid gap-6 rounded-lg border p-4 bg-white">
@@ -192,21 +258,70 @@ export default function KnowledgeSelect() {
                 <legend className="-ml-1 px-1 text-sm font-medium">
                   Skills
                 </legend>
-                <div className="grid grid-cols-1 gap-4">
-                  <div className="flex items-center">
-                    <Input
-                      id="level"
-                      type="number"
-                      min="0"
-                      max="3"
-                      defaultValue="0"
-                      className="w-min"
-                      // onChange={(e) => inputChange(e)}
-                    />
-                    <p className="pl-1">
-                      /3 <Label>Skullduggery</Label>
-                    </p>
-                  </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <fieldset className="grid gap-2 rounded-lg border p-2 bg-white">
+                    <legend className="flex -ml-1 px-1 text-sm font-medium">
+                      <Checkbox className="align-middle" />
+                      <p className="align-middle pl-1">Unlock: 10</p>
+                    </legend>
+                    <p className="justify-self-center font-bold">Survival</p>
+                    <div className="flex items-center">
+                      <Input
+                        id="level"
+                        type="number"
+                        min="0"
+                        max="3"
+                        defaultValue="0"
+                        className="w-12 h-8 p-2"
+                        // onChange={(e) => inputChange(e)}
+                      />
+                      <p className="pl-1">
+                        /3 <Label>Tracking</Label>
+                      </p>
+                    </div>
+                    <div className="flex items-center pl-4">
+                      <Input
+                        id="level"
+                        type="number"
+                        min="0"
+                        max="3"
+                        defaultValue="0"
+                        className="w-12 h-8 p-2"
+                        // onChange={(e) => inputChange(e)}
+                      />
+                      <p className="pl-1">
+                        /3 <Label>Tracking</Label>
+                      </p>
+                    </div>
+                    <div className="flex items-center pl-4">
+                      <Input
+                        id="level"
+                        type="number"
+                        min="0"
+                        max="3"
+                        defaultValue="0"
+                        className="w-12 h-8 p-2"
+                        // onChange={(e) => inputChange(e)}
+                      />
+                      <p className="pl-1">
+                        /3 <Label>Tracking</Label>
+                      </p>
+                    </div>
+                    <div className="flex items-center pl-4">
+                      <Input
+                        id="level"
+                        type="number"
+                        min="0"
+                        max="3"
+                        defaultValue="0"
+                        className="w-12 h-8 p-2"
+                        // onChange={(e) => inputChange(e)}
+                      />
+                      <p className="pl-1">
+                        /3 <Label>Tracking</Label>
+                      </p>
+                    </div>
+                  </fieldset>
                 </div>
               </fieldset>
               <fieldset className="grid gap-6 rounded-lg border p-4 bg-white">
